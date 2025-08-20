1b5fc9e848a93e6f48968758c0e235952ef83b57
prathik-singh-cem <prathik.singh@cemtics.com>
2025-08-21 01:37:33 +0530

    Uploading full FastAPI API project

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+"""
+T-Mobile Installation Cost Prediction API
+App package initialization
+"""
+
+__version__ = "1.0.0"
+__author__ = "T-Mobile Engineering Team"
+__description__ = "Enterprise-secure ML microservice for installation cost prediction"
